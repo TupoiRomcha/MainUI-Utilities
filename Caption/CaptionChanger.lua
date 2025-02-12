f31ea1fcc9392b4978cbec8540de5992a6d33b5e
Tupoi-official <155481068+TupoiRomcha@users.noreply.github.com>
2025-02-12 21:33:05 +0300

    Update and rename New Text Document.txt to CaptionChanger.lua

---
+++ Caption/CaptionChanger.lua
@@ -0,0 +1,8 @@
+local captionText = game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption
+-- Change
+captionText.Text = "amungsus"
+captionText.TextColor3 = Color3.fromRGB(255, 170, 127)
+captionText.BackgroundTransparency = 0.9
+captionText.BackgroundColor3 = Color3.fromRGB(255, 168, 105)
+captionText.Size = {0.4, 0}, {0.02, 10}
+captionText.Position = {0.5, 0}, {0.95, -60}
