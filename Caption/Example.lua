c8acf6c07e1cfb819dac0059606e49aa044e1076
Tupoi-official <155481068+TupoiRomcha@users.noreply.github.com>
2025-02-13 12:12:11 +0300

    Create Example.lua

---
+++ Caption/Example.lua
@@ -0,0 +1,8 @@
+local captionText = game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Caption
+-- Change
+captionText.Text = "amungsus"
+captionText.TextColor3 = Color3.fromRGB(0, 0, 0) -- set here your custom color for caption
+captionText.BackgroundTransparency = 1 -- set here your custom value, recommended 0.9 or 1
+captionText.BackgroundColor3 = Color3.fromRGB(0, 0, 0) -- if you maked BGTransparency to 0.9, you can change color
+-- captionText.Size = {0.4, 0}, {0.02, 10}
+-- captionText.Position = {0.5, 0}, {0.95, -60}
